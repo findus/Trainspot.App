0e1801f58eafc03357467e63364b85e1ad648473
Philipp <philipp@f1ndus.de>
2020-06-06 19:57:27 +0200

    Got timebased location almost working, even with stops, but needs more testing, and need to remove force unwraps

--- LocationManagerTest/LocationController/TrainLocationTripByTimeFrameController.swift
+++ LocationManagerTest/LocationController/TrainLocationTripByTimeFrameController.swift
@@ -36,16 +36,16 @@ class TrainLocationTripByTimeFrameController: TrainLocationProtocol  {
         dateComponents.year = 2020
         dateComponents.month = 6
         dateComponents.day = 4
-        dateComponents.hour = 14
-        dateComponents.minute = 55
-        dateComponents.second = 0
+        dateComponents.hour = 15
+        dateComponents.minute = 4
+        dateComponents.second = 00
         
         // Create date from components
         let userCalendar = Calendar.current // user calendar
         datestack.append(userCalendar.date(from: dateComponents)!)
         
-        dateComponents.minute = 55
-        dateComponents.second = 10
+        dateComponents.minute = 0
+        dateComponents.second = 40
         
         datestack.append(userCalendar.date(from: dateComponents)!)
     }
@@ -57,13 +57,14 @@ class TrainLocationTripByTimeFrameController: TrainLocationProtocol  {
     }
     
     private func startTrip(trip: TimeFrameTrip, withDate date: Date = Date()) {
-        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(test), userInfo: (trip, date), repeats: true)
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(test), userInfo: (trip, date), repeats: true)
     }
     
     @objc func test(timer: Timer) {
         var (trip, date) = timer.userInfo as! (TimeFrameTrip, Date)
         let coord = self.getTrainLocation(forTrip: trip, atDate: date.addingTimeInterval(i))
         i += 1
+        print( date.addingTimeInterval(i))
         self.delegate?.trainPositionUpdated(forTrip: trip, toPosition: coord, withDuration: 0)
     }
 
@@ -90,16 +91,65 @@ class TrainLocationTripByTimeFrameController: TrainLocationProtocol  {
 
 extension TrainLocationTripByTimeFrameController {
     func getTrainLocation(forTrip trip: TimeFrameTrip, atDate date: Date) -> CLLocation {
-        let (index,loc) = trip.locationArray.enumerated()
-            .filter( { $0.element.departure!.timeIntervalSince(date) <= 0 && $0.element.departure!.addingTimeInterval($0.element.durationToNext!).timeIntervalSince(date) >= 0  } ).first!
+        guard let loc = zip(trip.locationArray,trip.locationArray.dropFirst())
+            .first(where: { (this, next) -> Bool in
+                if this is Path && next is StopOver {
+                    /**
+                     Map Against Arrival Data:
+                    Section Start       Train     Arrival       Departure
+                        |__________*_____|     Stop     |________> Time .
+                      15:00              15:02    15:03          15:04
+                     */
+                    let result = (this as! Path).departure!.timeIntervalSince(date) <= 0 && (next as! StopOver).arrival!.timeIntervalSince(date) > 0
+                    if result == true {
+                        print("a")
+                    }
+                    return result
+                } else if this is StopOver && next is Path {
+                    /**
+                    Map Against Arrival Data:
+                    Section Start                     Arrival       Departure
+                        |________________|     Train     |________> Time .
+                    15:00                                 15:03          15:04
+                    */
+                    let result =
+                            ((this as! StopOver).arrival?.timeIntervalSince(date) ?? 1 <= 0 && (this as! StopOver).departure!.timeIntervalSince(date) > 0)
+                        ||
+                                (this as! StopOver).departure!.timeIntervalSince(date) <= 0 && (next).departure!.timeIntervalSince(date) >= 0
+                    if result == true {
+                        print("a")
+                    }
+                    return result
+                } else {
+                    let result =  this.departure!.timeIntervalSince(date) <= 0 && next.departure!.timeIntervalSince(date) > 0
+                    if result == true {
+                        print("a")
+                    }
+                    return result
+                }
+            }) else {
+                Log.error("Error finding a location for Trip \(trip.name) at \(date)")
+                fatalError()
+        }
         
-        let location = loc
+        
+        let location = loc.0
+        
+        // Check if currently stopping
+        
+        if location is StopOver {
+            let stopover = (location as! StopOver)
+            if stopover.arrival?.timeIntervalSince(date) ?? 1 <= 0 && stopover.departure!.timeIntervalSince(date) > 0 {
+                Log.debug("Currently halting til \(stopover.departure!) [\(stopover.departure!.timeIntervalSince(date)) seconds]")
+                return stopover.coords
+            }
+        }
         
         let wholeDuration = location.durationToNext!
         let departure = location.departure!
        
         let startCoords = location.coords.coordinate
-        let endCoords = trip.locationArray[index + 1].coords.coordinate
+        let endCoords = loc.1.coords.coordinate
         
         let secondsIntoSection = ( date.timeIntervalSince(departure) )
         
