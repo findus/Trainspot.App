3e360a27e795ca3f6882f0368acb7afcfcf740e8
Philipp <philipp@f1ndus.de>
2020-06-08 21:05:59 +0200

    wording

--- LocationManagerTest/LocationController/TrainLocationTripByTimeFrameController.swift
+++ LocationManagerTest/LocationController/TrainLocationTripByTimeFrameController.swift
@@ -198,7 +198,7 @@ extension TrainLocationTripByTimeFrameController {
         if location is StopOver {
             let stopover = (location as! StopOver)
             if stopover.arrival?.timeIntervalSince(date) ?? 1 <= 0 && stopover.departure!.timeIntervalSince(date) > 0 {
-                Log.debug("[\(trip.name)] Currently halting at: \(stopover.name) til \(stopover.departure!) [\(stopover.departure!.timeIntervalSince(date)) seconds]")
+                Log.debug("[\(trip.name)] Currently idling at: \(stopover.name) til \(stopover.departure!) [\(stopover.departure!.timeIntervalSince(date)) seconds]")
                 return stopover.coords
             }
         }
