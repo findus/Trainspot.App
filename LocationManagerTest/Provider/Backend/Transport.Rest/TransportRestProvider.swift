66f88107e4063aa17cc03a1f36362c2594ad75c2
Philipp <philipp@f1ndus.de>
2020-06-08 10:40:51 +0200

    Refactored cluttered stream code

--- LocationManagerTest/Provider/Backend/Transport.Rest/TransportRestProvider.swift
+++ LocationManagerTest/Provider/Backend/Transport.Rest/TransportRestProvider.swift
@@ -30,33 +30,54 @@ class TransportRestProvider: TrainDataProviderProtocol {
      
     func update() {
         
-        //let combined = Publishers.Merge(fetchArrivals(forStation: "8000049"), fetchDepartures(forStation: "8000049"))
-        fetchDepartures(forStation: "8000049")
+        let _ = fetchDepartures(forStation: "8000049")
         .merge(with: fetchArrivals(forStation: "8000049"))
         .collect()
         .map({ ( output : [Publishers.MergeMany<Future<Array<Journey>, AFError>>.Output]) -> Set<Journey> in
             Set(Array(output.joined()))
         })
-            .flatMap({ (journeys: Set<Journey>) -> Future<Array<JSON>, AFError> in
-            let futures = Array(journeys).map { (j:Journey) -> Future<JSON, AFError> in
-                self.fetchTrips(forJourney: j)
-            }
-                return Future { (completion) in
-                    Publishers.MergeMany(futures).collect().receive(on: RunLoop.main).sink(receiveCompletion: { (competion) in
-                    }) { (x: Array<JSON>) in
-                        completion(.success(x))
-                    }
-                }
+        .flatMap({ (journeys: Set<Journey>) -> Future<Array<JSON>, AFError> in
+                let futures = self.generateTripFutures(fromJourneys: journeys)
+                return self.fetch(trips: futures)
         })
         .receive(on: RunLoop.main)
-        .sink(receiveCompletion: { (onComplete) in
-                Log.info(onComplete)
+        .sink(receiveCompletion: { (result) in
+            switch result {
+            case .failure(let error):
+                Log.error(error)
+            case .finished:
+                Log.info(result)
+            }
             }) { (trips) in
                 let trips = trips.compactMap({ HafasParser.loadTimeFrameTrip2(fromJSON: $0) })
                 self.trips = trips
                 self.delegate?.onTripsUpdated()
         }
     }
+    
+    private func generateTripFutures(fromJourneys journeys: Set<Journey>) -> Array<Future<JSON, AFError>> {
+        return Array(journeys).map { (journey) -> Future<JSON, AFError> in
+            self.fetchTrip(forJourney: journey)
+        }
+    }
+    
+    private func fetch(trips fromFutures: Array<Future<JSON, AFError>>) -> Future<Array<JSON>, AFError> {
+        return Future { (completion) in
+            let _ = Publishers.MergeMany(fromFutures).collect().receive(on: RunLoop.current).sink(receiveCompletion: {(result) in
+                switch result {
+                case .failure(let error):
+                    completion(.failure(error))
+                case .finished:
+                    Log.info(result)
+                }
+            }) { (trips: Array<JSON>) in
+                completion(.success(trips))
+            }
+        }
+    }
+    
+    
+    // MARK: - Network code
     
     private func fetchDepartures(forStation id: String) -> Future<Array<Journey>, AFError> {
         
@@ -116,7 +137,7 @@ class TransportRestProvider: TrainDataProviderProtocol {
         
     }
     
-    private func fetchTrips(forJourney journey: Journey) -> Future<JSON, AFError> {
+    private func fetchTrip(forJourney journey: Journey) -> Future<JSON, AFError> {
          
          let headers = HTTPHeaders([HTTPHeader(name: "X-Identifier", value: "de.f1ndus.iOS.train")])
          
