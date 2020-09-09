e3f30d4d8cd523d68c3f049281801813f1ac70bd
Philipp <philipp@f1ndus.de>
2020-09-10 00:05:07 +0200

    Added Settings view controller

---
+++ LocationManagerTest/UI/SettingsComponents/DistanceToStationCell.swift
@@ -0,0 +1,24 @@
+//
+//  DistanceToStationCell.swift
+//  LocationManagerTest
+//
+//  Created by Philipp Hentschel on 09.09.20.
+//  Copyright © 2020 Philipp Hentschel. All rights reserved.
+//
+
+import UIKit
+
+class DistanceToStationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ LocationManagerTest/UI/SettingsComponents/SettingsTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsTableViewController.swift
+//  LocationManagerTest
+//
+//  Created by Philipp Hentschel on 10.09.20.
+//  Copyright © 2020 Philipp Hentschel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class SettingsTableViewController: UITableViewController  {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+}
